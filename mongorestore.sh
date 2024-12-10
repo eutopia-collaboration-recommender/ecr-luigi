f0b703c97d76f1ec8dbf9b58b091372f70c03dad
Luka Žontar <luka.zontar.consulting@gmail.com>
2024-12-10 21:28:24 +0000

    Added secrets to docker compose

--- mongorestore.sh
+++ mongorestore.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Waiting for MongoDB to be ready..."
-until mongo --username root --password rootpassword --eval "db.adminCommand('ping')"; do
+until mongo --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --eval "db.adminCommand('ping')"; do
   echo "MongoDB is not ready yet. Retrying in 2 seconds..."
   sleep 2
 done
 
 echo "MongoDB is ready. Restoring data..."
 
-mongorestore --uri "mongodb://root:rootpassword@localhost:27017" --archive=/dump/mongodump.dump
+# mongorestore --uri "mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@localhost:27017" --archive=/dump/mongodump.dump
 
 echo "MongoDB restore completed successfully."
