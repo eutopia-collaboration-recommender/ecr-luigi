03cc0738921123bba6c56563e95672025b3a1885
lukazontar <97096253+lukazontar@users.noreply.github.com>
2024-12-29 10:39:36 +0100

    Transforming is_new_collaboration to int

--- dbt/models/recommender/g_eucoht_node_article.sql
+++ dbt/models/recommender/g_eucoht_node_article.sql
@@ -1,6 +1,6 @@
 SELECT e.article_id,
        a.article_citation_normalized_count,
-       a.is_eutopia_collaboration,
+       CASE WHEN a.is_eutopia_collaboration THEN 1 ELSE 0 END AS is_eutopia_collaboration,
        a.collaboration_novelty_index,
        e.article_embedding
 FROM {{ ref("g_included_article_embedding") }} AS e
